531205a9a39981ca5dd24cb468b500218ab46923
iv-90 <videos10jue@gmail.com>
2019-11-01 09:48:10 -0500

    Autenticación de  usuarios 1

---
+++ resources/views/admin/admin/index.blade.php
@@ -0,0 +1,15 @@
+@extends("theme.$theme.layout")
+
+@section("titulo")
+Administrador
+@endsection
+
+@section('contenido')
+    
+    <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+            <h1 class="text-aqua text-center">Bienvenidos</h1>
+        </div>
+    </div>
+    
+@endsection
